a5a98843b51c7ac50cc98f28d5e95e6672164c50
William Henckel <williamhenckel@hotmail.com>
2022-11-03 18:24:21 +0100

    [main] homepage v2 + responsive tablet & desktop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 
 <template>
   <main>
-    <h2 class="text-3xl text-center mb-6 px-4">Retrouvez nos 3 suggestions du jours</h2>
+    <h2 class="text-3xl text-center mb-6 px-4">Retrouvez nos 3 suggestions du jour</h2>
     <CocktailsList />
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,13 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    // screens: {
-    //   'sm': '640px',
-    //   // => @media (min-width: 640px) { ... }
-
-    //   'md': '768px',
-    //   // => @media (min-width: 768px) { ... }
-
-    //   'lg': '1024px',
-    //   // => @media (min-width: 1024px) { ... }
-
-    //   'xl': '1280px',
-    //   // => @media (min-width: 1280px) { ... }
-
-    //   '2xl': '1536px',
-    //   // => @media (min-width: 1536px) { ... }
-    // },
+    screens: {
+      'sm': '425px',
+      'md': '768px',
+      // 'lg': '1024px',
+      // 'xl': '1280px',
+      // '2xl': '1536px',
+    },
 
     extend: {},
   },
